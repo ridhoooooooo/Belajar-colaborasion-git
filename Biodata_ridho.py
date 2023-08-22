b58616bcd0150d9b027a063fa2ef444aed795ec6
ridhoooooooo <139421513+ridhoooooooo@users.noreply.github.com>
2023-08-22 15:15:19 +0700

    Add files via upload

--- Biodata_ridho.py
+++ Biodata_ridho.py
@@ -2,8 +2,14 @@ print ("======================================================================")
 print ("Program Biodata Sederhana dengan python")
 print ("======================================================================")
 
-print   ("Nama   : ","M.RIDHO AL RASYID")
-print   ("Alamat : ","kec.Ampek Nagari kab.Agam Sumatra Barat")
-print   ("Hobi   : ","Sepak bola")
+
+nama = "M.RIDHO AL RASYID"
+hobi = "Sepak bola"
+alamat = "kec.Ampek Nagari kab.Agam Sumatra Barat"
+kota = "padang"
+
+print   ("Nama lengkap   : ",M.RIDHO AL RASYID)
+print   ("Alamat : ",kec.Ampek Nagari kab.Agam Sumatra Barat, "\n         kota", padang)
+print   ("Hobi   : "Sepak bola,)
 print   ("Selamat Datang di Gamelab Indonesia")
 print ("======================================================================")
